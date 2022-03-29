7028236a9c053ec0a74739f83ca2f2300c71a7fa
Shashank <shashank201011@gmail.com>
2022-03-29 19:00:26 +0100

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import useForm from "./formValidation";
 function App() {
+  //Custom hook call
+  const { handleChange, values, errors, checkbox, tigerField, handleSubmit } =
+    useForm();
+
+  const tigerCheckedBox = document.querySelectorAll(
+    "input[value=tiger]:checked"
+  ).length;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form id="sectionForm" method="post" action="" onSubmit={handleSubmit}>
+        <h1>Fill out this awesome form</h1>
+        <fieldset>
+          <h3>Your details</h3>
+
+          <p className={errors.email ? "error" : null}>
+            <label className="label" for="email">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              onChange={handleChange}
+            />
+          </p>
+          <p className={errors.password ? "error" : null}>
+            <label className="label" for="password">
+              Password
+            </label>
+            <input
+              className="error"
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChange}
+            />
+          </p>
+        </fieldset>
+
+        <fieldset>
+          <h3>Your animal</h3>
+          <p>
+            <label className="label" for="colour">
+              Colour
+            </label>
+            <select name="colour" id="colour" required>
+              <option value="">Choose colour</option>
+              <option value="blue">Blue</option>
+              <option value="green">Green</option>
+              <option value="red">Red</option>
+              <option value="black">Black</option>
+              <option value="brown">Brown</option>
+            </select>
+          </p>
+          <p className={checkbox.checkbox ? "error" : null}>
+            <span className="label">Animal</span>
+
+            <input
+              type="checkbox"
+              name="animal"
+              value="bear"
+              id="bear"
+              onChange={handleChange}
+            />
+            <label for="bear">Bear</label>
+
+            <input
+              type="checkbox"
+              name="animal"
+              value="tiger"
+              id="tiger"
+              onChange={handleChange}
+            />
+            <label for="tiger">Tiger</label>
+
+            <input
+              type="checkbox"
+              name="animal"
+              value="snake"
+              id="snake"
+              onChange={handleChange}
+            />
+            <label for="snake">Snake</label>
+
+            <input
+              type="checkbox"
+              name="animal"
+              value="donkey"
+              id="donkey"
+              onChange={handleChange}
+            />
+            <label for="donkey">Donkey</label>
+          </p>
+          <p className={tigerField.tigerInput ? "error" : null}>
+            <label className="label" for="tiger_type">
+              Type of tiger
+            </label>
+
+            <input
+              type="text"
+              name="tiger_type"
+              id="tiger_type"
+              disabled={tigerCheckedBox === 1 ? false : true}
+              onChange={handleChange}
+            />
+          </p>
+        </fieldset>
+        <fieldset>
+          <p>
+            <input type="submit" value="Create account" />
+          </p>
+        </fieldset>
+      </form>
     </div>
   );
 }
---
+++ src/formValidation.js
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from "react";
+import { omit } from "lodash";
+
+const useForm = () => {
+  //Form values
+  const [values, setValues] = useState({});
+  //Errors
+  const [errors, setErrors] = useState({});
+  const [checkbox, setCheckbox] = useState({});
+  const [tigerField, setTigerField] = useState({});
+
+//   const tigerCheckedBox = document.querySelectorAll("input[name=tiger_type]");
+
+  const validate = (event, name, value) => {
+    switch (name) {
+      case "email":
+        if (
+          !new RegExp(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          ).test(value)
+        ) {
+          setErrors({
+            ...errors,
+            email: "Enter a valid email address",
+          });
+        } else {
+          let newObj = omit(errors, "email");
+          setErrors(newObj);
+        }
+        break;
+
+      case "password":
+        if (value.length <= 7) {
+          setErrors({
+            ...errors,
+            password: "Password should contains atleast 8 charaters",
+          });
+        } else {
+          const newObj = omit(errors, "password");
+
+          setErrors(newObj);
+          //console.log(errors);
+        }
+        break;
+
+      case "animal":
+        const checkedBoxes = document.querySelectorAll(
+          "input[name=animal]:checked"
+        );
+        var boxesTicked = 0;
+        for (let i = checkedBoxes.length - 1; i >= 0; i--) {
+          if (checkedBoxes[i].checked) {
+            boxesTicked++;
+          }
+        }
+
+        if (boxesTicked > 1) {
+          //console.log("1");
+          const newObj = omit(checkbox, "checkbox");
+          setCheckbox(newObj);
+
+          //console.log( newObj);
+        } else {
+          //console.log("2");
+          setCheckbox({
+            ...checkbox,
+            checkbox: "You must select at least two",
+          });
+        }
+
+        break;
+
+      case "tiger_type":
+        const tigerCheckedBox = document.querySelectorAll(
+          "input[name=tiger_type]"
+        )[0].disabled;
+
+        const tigerInput = document.querySelectorAll("input[name=tiger_type]");
+
+        if (
+          //false
+          !tigerCheckedBox &&
+          tigerInput[0].value
+        ) {
+          const newObj = omit(tigerField, "tiger_type");
+          setTigerField(newObj);
+          console.log("1");
+        } else {
+          console.log("2");
+          setTigerField({
+            ...tigerField,
+            tigerInput: "Please input a tiger type ",
+          });
+        }
+
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  //A method to handle form inputs
+  const handleChange = (event) => {
+    //To stop default events
+    event.persist();
+
+    let name = event.target.name;
+    let val = event.target.value;
+
+    validate(event, name, val);
+    //Let's set these values in state
+
+    setValues({
+      ...values,
+      [name]: val,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault();
+    const tigerCheckedBox = document.querySelectorAll(
+      "input[name=tiger_type]"
+    )[0].disabled;
+
+    // console.log(
+    //   //values,
+    //   //   tigerField,
+    //   Object.keys(errors).length,
+    //   Object.keys(checkbox).length,
+    //   Object.keys(tigerField).length,
+    //   Object.keys(values).length,
+    //   tigerCheckedBox
+    // );
+
+    if (!tigerCheckedBox) {
+      if (
+        Object.keys(errors).length === 0 &&
+        Object.keys(checkbox).length === 0 &&
+        Object.keys(tigerField).length === 0 &&
+        Object.keys(values).length === 4
+      ) {
+        alert("Form Submitted!!");
+      } else {
+        alert("FIll in the whole form");
+      }
+    } else {
+      if (
+        Object.keys(errors).length === 0 &&
+        Object.keys(checkbox).length === 0 &&
+        Object.keys(tigerField).length === 0 &&
+        Object.keys(values).length === 3
+      ) {
+        alert("Form Submitted!!!");
+      } else {
+        alert("FIll in the whole form");
+      }
+    }
+  };
+
+  return {
+    values,
+    errors,
+    checkbox,
+    tigerField,
+    handleChange,
+    handleSubmit,
+  };
+};
+
+export default useForm;
